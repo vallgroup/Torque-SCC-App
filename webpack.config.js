1e37e00c2aafe08f65a98a51c1d9da05f68d607e
mattwills8 <matt_wills8@hotmail.co.uk>
2019-04-08 13:52:58 -0600

    add eslint w/ airbnb config as a base

--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,77 @@
-const projectConfig = require("../../../config");
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const projectConfig = require('../../../config');
 
-const srcDir = path.join(__dirname, "src");
-const buildDir = path.join(projectConfig.root, "wp-content/themes/scc-child");
+const srcDir = path.join(__dirname, 'src');
+const buildDir = path.join(projectConfig.root, 'wp-content/themes/scc-child');
 
 const config = {
   context: srcDir,
 
   entry: {
-    main: ["@babel/polyfill/noConflict", "./index.js"]
+    main: ['@babel/polyfill/noConflict', './index.js'],
   },
 
   devServer: {
     historyApiFallback: true,
     hot: true,
     overlay: true,
-    port: 8100
+    port: 8100,
   },
 
   output: {
-    path: path.join(buildDir, "./bundles/app"),
-    publicPath: "/",
-    filename: "[name].[hash].js"
+    path: path.join(buildDir, './bundles/app'),
+    publicPath: '/',
+    filename: '[name].[hash].js',
   },
 
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
-          fallback: "style-loader?sourceMap",
+          fallback: 'style-loader?sourceMap',
           use: [
             'css-loader?modules&importLoaders=1&localIdentName="[local]__[hash:base64:5]"',
-            "postcss-loader"
-          ]
-        })
+            'postcss-loader',
+          ],
+        }),
       },
       {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
-          fallback: "style-loader?sourceMap",
+          fallback: 'style-loader?sourceMap',
           // resolve-url-loader may be chained before sass-loader if necessary
           use: [
             'css-loader?modules&importLoaders=1&localIdentName="[local]__[hash:base64:5]"',
-            "sass-loader?sourceMap",
-            "postcss-loader"
-          ]
-        })
+            'sass-loader?sourceMap',
+            'postcss-loader',
+          ],
+        }),
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
         use: {
-          loader: "url-loader",
+          loader: 'url-loader',
           options: {
-            limit: 8192
-          }
-        }
+            limit: 8192,
+          },
+        },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: "file-loader"
-      }
-    ]
+        use: 'file-loader',
+      },
+    ],
   },
 
   optimization: {
@@ -79,27 +79,27 @@ const config = {
       cacheGroups: {
         commons: {
           test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all"
-        }
-      }
-    }
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      title: "SCC",
-      template: path.join(srcDir, "index.html")
+      title: 'SCC',
+      template: path.join(srcDir, 'index.html'),
     }),
     new ExtractTextPlugin({
-      filename: "main.css",
-      publicPath: "/",
+      filename: 'main.css',
+      publicPath: '/',
       allChunks: true,
-      ignoreOrder: true
-    })
+      ignoreOrder: true,
+    }),
   ],
 
-  devtool: "source-map"
+  devtool: 'source-map',
 };
 
 module.exports = config;
