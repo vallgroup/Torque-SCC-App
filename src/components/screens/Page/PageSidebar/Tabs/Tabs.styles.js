29fd2227954b1b2e723621cd053ca9104e64683f
mattwills8 <matt_wills8@hotmail.co.uk>
2019-04-19 15:41:49 -0600

    update sidebar tabs style

--- src/components/screens/Page/PageSidebar/Tabs/Tabs.styles.js
+++ src/components/screens/Page/PageSidebar/Tabs/Tabs.styles.js
@@ -12,14 +12,16 @@ export const TabsRoot = styled.div`
 
 export const Tab = styled.div`
   float: left;
-  border: 2px solid ${({ theme }) => theme.colors.white};
+
+  margin-top: -0.1vw; /* overlap borders */
+  border: 0.1vw solid ${({ theme }) => theme.colors.white};
   padding: 2% 5%;
   min-width: 25%;
   box-sizing: border-box;
 
   text-transform: uppercase;
 
-  font-size: 0.6vw;
+  font-size: 0.8vw;
   font-weight: 500;
   text-align: center;
 
