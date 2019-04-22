55628f3ac71016e7e8573017d2669c4a607011d3
mattwills8 <matt_wills8@hotmail.co.uk>
2019-04-22 16:47:14 -0600

    update map scroll bar styles

--- src/components/screens/Page/PageSidebar/TabPois/TabPois.styles.js
+++ src/components/screens/Page/PageSidebar/TabPois/TabPois.styles.js
@@ -3,6 +3,22 @@ import styled from 'styled-components';
 export const ContentRoot = styled.div`
   max-height: 60%;
   overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    width: 1em;
+  }
+  &::-webkit-scrollbar-track {
+    -webkit-box-shadow: transparent;
+    background: transparent;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(225, 225, 225, 0.4);
+    outline: 2px solid white;
+    border-radius: 100px;
+  }
+  &::-webkit-scrollbar-corner {
+    background-color: transparent;
+  }
 `;
 
 export const PoiWrapper = styled.div`
